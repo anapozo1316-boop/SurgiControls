df46ebccdcaa482487c4ad7b2e209ffb53e98052
rosana.sanchez <anapozo1316@gmail.com>
2026-05-27 01:09:53 -0500

    Add files via upload

--- database/db.py
+++ database/db.py
@@ -1,11 +1,14 @@
 import sqlite3
 
 def get_connection():
+
     conn = sqlite3.connect(
         "data/surgicontrol.db",
         check_same_thread=False
     )
 
-    conn.execute("PRAGMA foreign_keys = ON")
+    conn.execute(
+        "PRAGMA foreign_keys = ON"
+    )
 
     return conn
